fd7058f2f5042f25b0668e85b4176836e7196615
Túlio Cota <tulio.cota@nerdweb.com.br>
2022-11-27 09:28:14 -0300

    Página de erro 404

---
+++ 404.php
@@ -0,0 +1,44 @@
+<style>
+    html {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        min-height: 100%;
+    }
+
+    .card-outer {
+        display: flex;
+        justify-content: center;
+        min-height: 100vh;
+    }
+
+    .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        transition: 0.3s;
+        width: 16%;
+        float: left;
+        margin-left: 12px;
+        align-self: center;
+    }
+
+    .card:hover {
+        box-shadow: 0 6px 14px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    .container {
+        padding: 38px 40px;
+    }
+</style>
+<div class="card-outer">
+    <div class="card">
+        <div class="container">
+            <h1 style="text-align:center;">404</h1>
+            <p style="text-align:center;">
+                Ops! Página não encontrada
+            </p>
+        </div>
+    </div>
+</div>
